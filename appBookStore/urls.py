e7c279200b52eacd48051474093f5b0e2c640dc1
juan-ud-santos <j.santos@opendeusto.es>
2019-11-17 16:53:07 +0100

    Ultimos updates pre entrega 2

--- appBookStore/urls.py
+++ appBookStore/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('bookstore/', views.index, name='home'),
-path('bookstore/examples', views.examples, name='examples'),
-path('bookstore/a_page', views.a_page, name='a_page'),
-path('bookstore/another_page', views.another_page, name='another_page'),
-path('bookstore/contact', views.contact, name='contact'),
-path('bookstore/register', views.register, name='register'),
-path('bookstore/login', views.login, name='login'),
-path('bookstore/details/<ID>', views.details, name='details'),
-path('bookstore/addMensaje/', views.addMensaje, name='addMensaje'),
-path('bookstore/addUser/', views.addUser, name='addUser')
+	path('', views.index, name='home'),
+	path('examples', views.examples, name='examples'),
+	path('contact', views.contact, name='contact'),
+	path('register', views.register, name='register'),
+	path('login', views.login, name='login'),
+	path('details/<ID>', views.details, name='details'),
+	path('addMensaje/', views.addMensaje, name='addMensaje'),
+	path('addUser/', views.addUser, name='addUser')
 ]
